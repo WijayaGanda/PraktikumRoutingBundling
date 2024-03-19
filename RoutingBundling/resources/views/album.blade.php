4007a1254901e4f13d854f78d9805a69c213e8d6
WijayaGanda <64698425+WijayaGanda@users.noreply.github.com>
2024-03-19 21:30:21 +0700

    Add files via upload
    Wijaya Ganda Prasetyo 1204220048

---
+++ RoutingBundling/resources/views/album.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/app.css">
+  <title>Home</title>
+</head>
+<body>
+  <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary bg-purple ">
+    <div class="container-fluid ">
+      <a class="navbar-brand" href="#" style="color: gold;">Wijaya Ganda </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="/home" style="color: white;">Home</a>
+          <a class="nav-link" href="/album" style="color: white;">Album</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="../images/rplno1.jpg" class="card-img-top" alt="...">
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../images/pialalomba.jpg" class="card-img-top" alt="...">
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../images/fotolomba.JPG" class="card-img-top" alt="...">
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../images/fotobgbanner.jpg" class="card-img-top" alt="...">
+      </div>
+    </div>
+  </div>
+
+  <div class="card text-center mt-5">
+    <div class="card-header bg-black borderbottom" style="color: gold;">
+      Wijaya Ganda Prasetyo-1204220048
+    </div>
+    <div class="card-body bg-black" style="color: gold;">
+      <h5 class="card-title">Sistem Informasi</h5>
+      <p class="card-text">Telkom University Surabaya</p>
+      <a href="/album" class="btn btn-primary">Go Up</a>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ RoutingBundling/resources/views/home.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/app.css">
+  <title>Home</title>
+</head>
+<body>
+  <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary bg-purple ">
+    <div class="container-fluid ">
+      <a class="navbar-brand" href="#" style="color: gold;">Wijaya Ganda </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="/home" style="color: white;">Home</a>
+          <a class="nav-link" href="/album" style="color: white;">Album</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="card text-bg-dark">
+    <img src="../images/fotolomba.JPG" class="card-img" alt="...">
+    <div class="card-img-overlay">
+      <p class="card-text text-center mt-5" style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; font-size:30px">Halo, Saya Wijaya Ganda Prasetyo</p>
+      <p class="card-text text-center 2" style="font-family: 'Times New Roman', Times, serif; font-size:20px;">Saya adalah mahasiswa semester 4 di Telkom University Surabaya Prodi Sistem Informasi</p>
+      <p class="card-text text-center mt-2" style="font-family: 'Times New Roman', Times, serif; font-size:20px">Saya memiliki minat di dunia pemrograman sejak SMK setelah mengambil Jurusan</p>
+      <p class="card-text text-center mt-2" style="font-family: 'Times New Roman', Times, serif; font-size:20px">Rekayasa Perangkat Lunak di rumpun Informatika</p>
+    </div>
+  </div>
+  <h1 class="mt-5" style="text-align: center;">Achievements</h1>
+  <div class="row">
+    <div class="card mb-3 mt-5" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="../images/piala-sertiflomba.jpg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">PESERTA TERBAIK IT SOFTWARE SOLUTION FOR BUSINESS</h5>
+            <p class="card-text">Peserta terbaik Lomba kompetensi siswa SMK tingkat Wilker 2 Bidang lomba IT SOFTWARE SOLUTION FOR BUSINESS tahun 2022 </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-3 mt-5" style="max-width: 440px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="../images/rplno1.jpg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">WISUDAWAN TERBAIK JURUSAN REKAYASA PERANGKAT LUNAK</h5>
+            <p class="card-text">Wisudawan terbaik jurusan RPL Tahun 2022 di SMK Negeri 1 Purwosari Pasuruan</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-3 mt-5" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="../images/rekapnilali.jpg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">HARAPAN 1 TINGKAT PROVINSI</h5>
+            <p class="card-text">Peringkat 4 lomba kompetensi siswa SMK bidang lomba IT SOFTWARE SOLUTION FOR BUSINESS tahun 2022 Tingkat Provinsi Jawa Timur di SMKN 2 Bangkalan Madura</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card text-center mt-5">
+    <div class="card-header bg-black borderbottom" style="color: gold;">
+      Wijaya Ganda Prasetyo-1204220048
+    </div>
+    <div class="card-body bg-black" style="color: gold;">
+      <h5 class="card-title">Sistem Informasi</h5>
+      <p class="card-text">Telkom University Surabaya</p>
+      <a href="/home" class="btn btn-primary">Go Up</a>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
